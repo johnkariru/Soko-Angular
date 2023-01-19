174a4b23af29ca8c2f1b8abaabf27e01d08c9ac8
johnmukii <johnmukii@yahoo.com>
2023-01-19 10:43:30 +0300

    after   forking

---
+++ src/app/mobile/mobile_home/mobile_home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-mobile_home',
+  templateUrl: './mobile_home.component.html',
+  styleUrls: ['./mobile_home.component.css']
+})
+export class MobileHomeComponent {
+
+
+
+  constructor() { }
+
+
+
+}
